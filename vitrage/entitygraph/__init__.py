69fdccbd3c74fe1ea1aed5d65383749f774c48a5
Alexey Weyl <alexey.weyl@alcatel-lucent.com>
2015-11-16 00:16:14 +0000

    Add common directories and initializers
    Change-Id: Ia5df79c54b1ac0185354d75f3958e157c6029a64

--- vitrage/entitygraph/__init__.py
+++ vitrage/entitygraph/__init__.py
@@ -12,8 +12,4 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import pbr.version
-
-
-__version__ = pbr.version.VersionInfo(
-    'vitrage').version_string()
+__author = 'stack'
