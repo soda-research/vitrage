2b8474972064705e2b3c4736804bf3cd21a29968
Liat Har-Tal <liat.har-tal@alcatel-lucent.com>
2015-11-30 09:27:40 +0000

    entity graph - properties file
    Change-Id: I4af9abbfd3aa1f0653ed821f0ce853cd9155f232

---
+++ vitrage/entity_graph/properties.py
@@ -0,0 +1,30 @@
+# Copyright 2015 - Alcatel-Lucent
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+# Vertex properties
+TYPE = 'TYPE'
+SUB_TYPE = 'SUB_TYPE'
+ID = 'ID'
+VERTEX_ID = 'VERTEX_ID'
+EVENT_TYPE = 'EVENT_TYPE'
+IS_VERTEX_DELETED = 'IS_VERTEX_DELETED'
+VERTEX_DELETION_TIMESTAMP = 'VERTEX_DELETION_TIMESTAMP'
+STATE = 'STATE'
+PROJECT = 'PROJECT'
+TIMESTAMP = 'TIMESTAMP'
+
+# Edge properties
+RELATION_NAME = 'RELATION_NAME'
+IS_EDGE_DELETED = 'IS_EDGE_DELETED'
+EDGE_DELETION_TIMESTAMP = 'EDGE_DELETION_TIMESTAMP'
