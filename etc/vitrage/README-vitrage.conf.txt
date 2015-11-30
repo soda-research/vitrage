cb4e3be14efa79a935799cc46e353636c1dcc0d7
Eyal <eyal.bar-ilan@alcatel-lucent.com>
2015-11-30 11:44:37 +0200

    add configuration files and options
    Implements: blueprint get-topology-api
    Change-Id: Id0399997cfe23975774a828cbc6e608484f0b988

---
+++ etc/vitrage/README-vitrage.conf.txt
@@ -0,0 +1,4 @@
+To generate the sample vitrage.conf file, run the following
+command from the top-level vitrage directory:
+
+tox -egenconfig
--- vitrage/opts.py
+++ vitrage/opts.py
@@ -15,6 +15,17 @@ from oslo_config import cfg
 
 def list_opts():
     return [("api", (
+        cfg.PortOpt('port',
+                    default=8999,
+                    help='The port for the vitrage API server.',
+                    ),
+        cfg.StrOpt('host',
+                   default='0.0.0.0',
+                   help='The listen IP for the vitrage API server.',
+                   ),
+        cfg.StrOpt('paste_config', default="api-paste.ini",
+                   help="Configuration file for WSGI definition of API."),
+
         cfg.IntOpt('workers', default=1,
                    min=1,
                    help='Number of workers for vitrage API server.'),
