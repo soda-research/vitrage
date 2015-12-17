4c34e7867785fcf47de7f7289a4055f428f2a12a
Eyal <eyal.bar-ilan@alcatel-lucent.com>
2015-12-17 13:08:14 +0200

    fix import
    Change-Id: I1f18867c2adfc3977cbeb71b24937ce0b56b576a
    Implements: blueprint get-topology-api

--- vitrage/api/policy.py
+++ vitrage/api/policy.py
@@ -12,7 +12,7 @@
 
 from pecan.core import abort
 
-from common.utils import recursive_keypairs
+from vitrage.common.utils import recursive_keypairs
 
 
 def enforce(rule, headers, enforcer, target):
