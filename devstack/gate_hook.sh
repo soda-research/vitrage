eb76d8f2e545572ba96842ba2041c52fa5070439
Noam Bloom <noam.bloom.ext@nokia.com>
2016-05-08 11:59:31 +0300

    Tempest Gate - Update
    1. Remove DEVSTACK_GATE_NO_SERVICES since it disables all default services
    (see devstack-gate/features.yaml no_services variable.)
    2. Remove redundant imports, ceilometer is enabled by default as a service(see 1.) and
     adding AODH project is already done in vitrage.yaml under project-config/jenkins/jobs
    
    Change-Id: I755ca121bdfc02b0b34bbb9d34a000a34d36128c

--- devstack/gate_hook.sh
+++ devstack/gate_hook.sh
@@ -15,18 +15,13 @@
 
 export DEVSTACK_GATE_NEUTRON=1
 export DEVSTACK_GATE_INSTALL_TESTONLY=1
-export DEVSTACK_GATE_NO_SERVICES=1
 export DEVSTACK_GATE_TEMPEST=0
-export DEVSTACK_GATE_EXERCISES=0
 export KEEP_LOCALRC=1
 
-
-export PROJECTS="openstack/aodh $PROJECTS"
 
 if [ -z ${DEVSTACK_LOCAL_CONFIG+x} ]; then
     DEVSTACK_LOCAL_CONFIG="enable_plugin vitrage git://git.openstack.org/openstack/vitrage"
 fi
-DEVSTACK_LOCAL_CONFIG+=$'\nenable_plugin ceilometer git://git.openstack.org/openstack/ceilometer'
 DEVSTACK_LOCAL_CONFIG+=$'\nenable_plugin aodh git://git.openstack.org/openstack/aodh'
 DEVSTACK_LOCAL_CONFIG+=$'\ndisable_service ceilometer-alarm-evaluator,ceilometer-alarm-notifier'
 DEVSTACK_LOCAL_CONFIG+=$'\ndisable_service n-net'
