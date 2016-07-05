159da54244cf4ac77695ebdbfe5a21b5e7529ed7
Noam Bloom <noam.bloom.ext@nokia.com>
2016-07-05 09:51:57 +0300

    Gate-hook devstack post-config
    Changed datasources snapshot interval(get_all) to two hours
     instead of 10 min. This is to avoid having a difference in sample time
     during tempests. Given that the tempests run in an isolated environment
     and that all changes occur from within the tempests, the change should not affect
     current tempests consistency.
    
    Change-Id: I2c238275c8ffee0fc24915e98b49ee946a71069d

--- devstack/gate_hook.sh
+++ devstack/gate_hook.sh
@@ -58,6 +58,10 @@ notification_driver = messagingv2
 [DEFAULT]
 notification_topics = notifications,vitrage_notifications
 notification_driver = messagingv2
+
+[[post-config|\$VITRAGE_CONF]]
+[datasources]
+snapshots_interval = 7200
 EOF
 
 $GATE_DEST/devstack-gate/devstack-vm-gate.sh
