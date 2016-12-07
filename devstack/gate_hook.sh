bd02779a9c3cef1cedabffa42c97b30969d10de4
Alexey Weyl <alexey.weyl@nokia.com>
2016-12-07 13:20:35 +0200

    enable heat in gate
    Change-Id: I521ed424aba9256dfa1fc4c7db45394cd2cd84d7

--- devstack/gate_hook.sh
+++ devstack/gate_hook.sh
@@ -24,6 +24,7 @@ export KEEP_LOCALRC=1
 if [ -z ${DEVSTACK_LOCAL_CONFIG+x} ]; then
     DEVSTACK_LOCAL_CONFIG="enable_plugin vitrage git://git.openstack.org/openstack/vitrage"
 fi
+DEVSTACK_LOCAL_CONFIG+=$'\nenable_plugin heat git://git.openstack.org/openstack/heat'
 DEVSTACK_LOCAL_CONFIG+=$'\nenable_plugin ceilometer git://git.openstack.org/openstack/ceilometer'
 DEVSTACK_LOCAL_CONFIG+=$'\nenable_plugin aodh git://git.openstack.org/openstack/aodh'
 DEVSTACK_LOCAL_CONFIG+=$'\ndisable_service ceilometer-alarm-evaluator,ceilometer-alarm-notifier'
