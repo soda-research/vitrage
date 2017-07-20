2cacde9a98a265db89d8111ee64223917dd038f9
rajat29 <rajat.sharma@nectechnologies.in>
2017-07-20 18:53:35 +0530

    Replace six.itervalues with dict.values()
    We should avoid using six.itervalues to achieve iterators.
    We can use dict.values instead, as it willreturn iterators
    in PY3 as well. And dict.items/values will more
    readable.
    
    Change-Id: Ic9a0fe2edd939ca28bdc634f5ee92ec2546ddd29

--- vitrage/graph/driver/elements.py
+++ vitrage/graph/driver/elements.py
@@ -11,7 +11,6 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-import six
 
 
 class PropertiesElement(object):
@@ -34,7 +33,7 @@ class PropertiesElement(object):
             del self.properties[key]
 
     def __iter__(self):
-        return six.itervalues(self.properties)
+        return self.properties.values()
 
     def get(self, k, d=None):
         return self.properties.get(k, d)
