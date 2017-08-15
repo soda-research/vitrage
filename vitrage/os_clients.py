7cd46bd71e612f539b9c4da826676b5b112f5457
Anna <anna.reznikov@nokia.com>
2017-08-15 08:12:44 +0000

    change the way heat client is created
    Closes-Bug: #1710473
    
    Change-Id: I95071b1a552ab2ea7de0ac2f88ca5b80f642ac73

--- vitrage/os_clients.py
+++ vitrage/os_clients.py
@@ -12,8 +12,6 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import keystoneauth1.identity.v2 as v2
-import keystoneauth1.session as kssession
 from oslo_config import cfg
 from oslo_log import log
 from oslo_utils import importutils as utils
@@ -136,12 +134,7 @@ def heat_client(conf):
     """Get an instance of heat client"""
     # auth_config = conf.service_credentials
     try:
-        auth = v2.Password(
-            auth_url=conf.service_credentials.auth_url + '/v2.0',
-            username=conf.service_credentials.username,
-            password=conf.service_credentials.password,
-            tenant_name=conf.service_credentials.project_name)
-        session = kssession.Session(auth=auth)
+        session = keystone_client.get_session(conf)
         endpoint = session.get_endpoint(service_type='orchestration',
                                         interface='publicURL')
         he_client = driver_module('heat')
