3d37a3ea661bc55d85a0bc5a37f66b6f71889c0d
Eyal <eyal.bar-ilan@nokia.com>
2018-02-22 11:29:41 +0200

    move to stestr
    testr is not maintained any more
    
    Depends-On: I574414aba16377cc550111b9bcd627c28f04fc49
    Change-Id: Ibea81c6583300074cfed3c1fa7fb98e2d1dc90fc

--- devstack/post_test_hook.sh
+++ devstack/post_test_hook.sh
@@ -31,9 +31,9 @@ fi
 sudo cp -rf $DEVSTACK_PATH/tempest/etc/logging.conf.sample $DEVSTACK_PATH/tempest/etc/logging.conf
 
 cd $DEVSTACK_PATH/tempest/
-sudo -E testr init
+sudo -E stestr init
 
 echo "Listing existing Tempest tests"
-sudo -E testr list-tests vitrage_tempest_tests | grep -E "$TESTS" | tee /tmp/vitrage_tempest_tests.list
+sudo -E stestr list vitrage_tempest_plugin | grep -E "$TESTS" | tee /tmp/vitrage_tempest_tests.list
 echo "Testing $1: $TESTS..."
-sudo -E testr run --subunit --load-list=/tmp/vitrage_tempest_tests.list | subunit-trace --fails
+sudo -E stestr run --serial --subunit --load-list=/tmp/vitrage_tempest_tests.list | subunit-trace --fails
